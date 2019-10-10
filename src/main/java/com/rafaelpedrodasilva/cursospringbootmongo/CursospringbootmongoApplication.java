7d8207d64fc63f85d79fd6773d275424ac7a1bc6
Rafael Pedro da Silva <rafaelpedrodasilva@outlook.com>
2019-10-10 14:45:58 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/rafaelpedrodasilva/cursospringbootmongo/CursospringbootmongoApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelpedrodasilva.cursospringbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringbootmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringbootmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelpedrodasilva/cursospringbootmongo/CursospringbootmongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rafaelpedrodasilva.cursospringbootmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursospringbootmongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
