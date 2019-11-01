53243c3c4a3fecef6855ab00b4ab517bdb443212
Rafael Pedro da Silva <rafaelpedrodasilva@outlook.com>
2019-11-01 09:27:44 -0300

    Consulta simples com @Query

--- src/main/java/com/rafaelpedrodasilva/cursospringbootmongo/repository/PostRepository.java
+++ src/main/java/com/rafaelpedrodasilva/cursospringbootmongo/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.rafaelpedrodasilva.cursospringbootmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rafaelpedrodasilva.cursospringbootmongo.entities.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ title: { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitleUsingRegex(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/rafaelpedrodasilva/cursospringbootmongo/services/PostService.java
+++ src/main/java/com/rafaelpedrodasilva/cursospringbootmongo/services/PostService.java
@@ -30,6 +30,7 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+//		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.findByTitleUsingRegex(text);
 	}
 }
